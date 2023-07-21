ad50a454e37ef5047015a69b6e315a0880685938
Sam Puddister <sampuddister@gmail.com>
2023-07-21 16:03:14 -0400

    Added link to store on home page.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,25 @@
 import "./Home.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
-      <div className="logo-container">
-        <div className="logo">F</div>
+      <div className="content">
+        <div className="logo-container">
+          <div className="logo">F</div>
+        </div>
+        <div className="visit-store-wrapper">
+          <Link className="visit-store-link" to={"/store"}>
+            Click here to visit the store
+          </Link>
+        </div>
       </div>
       <div className="banner">
         <div className="banner-message">
           <p className="message-one">real stuff...</p>
           <p className="message-two">...fake store</p>
-          <p className="message-three">Shop Now!</p>
+          <p className="message-three">Pretend to shop now!</p>
         </div>
       </div>
     </div>
